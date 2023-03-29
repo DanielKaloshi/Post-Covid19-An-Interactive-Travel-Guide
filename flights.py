b9e2a89e76f2369e7715ebdb92ba9d4000bb8406
Anson Lau <ansonlau57@gmail.com>
2023-03-29 12:06:33 -0400

    Update flights.py

--- flights.py
+++ flights.py
@@ -1,1 +1,102 @@
-# Anson
+from __future__ import annotations
+from typing import Any, Optional
+
+
+class Country:
+    """A vertex that represents a country in the flights network.
+
+    Instance Attributes:
+    - name:
+        The name of this country.
+    - neighbours:
+        A mapping containing the neighbours of this country. A country is a neighbour if there
+        is a direct flight between self and that country.
+        Each key in the mapping is the name of a neighbouring country, and the corresponding
+        value is the vertex associated with that country.
+    - safety_index:
+        The calculated safety index for this country.
+    - region:
+        The WHO region this country is located in.
+
+    Representation Invariants:
+    - self.name not in self.neighbours
+    - all(self.name in country.neighbours for country in self.neighbours.values())
+    """
+    name: str
+    neighbours: dict[str, Country]
+    safety_index: float
+    region: str
+
+    def __init__(self, name: str, neighbours: dict[str, Country], region: str) -> None:
+        """Initialize this country with the given name, region, and neighbours."""
+        self.name = name
+        self.neighbours = neighbours
+        self.region: region
+
+    def check_connected(self, target_country: str, visited: set[Country]) -> bool:
+        """Return whether this country is connected to a country corresponding to the target_country,
+        WITHOUT using any of the countries in visited.
+
+        Preconditions:
+            - self not in visited
+        """
+        if self.name == target_country:
+            return True
+        else:
+            visited.add(self)
+            for neighbour in self.neighbours.values():
+                if neighbour not in visited:
+                    if neighbour.check_connected(target_country, visited):
+                        return True
+            return False
+
+
+class Flights:
+    """A network representing the available flight paths around the world.
+
+    Instance Attributes:
+    - countries
+        A collection of the countries connected by flights in this network.
+        Maps the name to the Country object.
+
+    Representation Invariants:
+    - all(country == self.countries[country].name for country in self.countries)
+    """
+    countries: dict[str, Country]
+
+    def __init(self) -> None:
+        """Initialize an empty flight network."""
+        self.countries = {}
+
+    def add_country(self, name: str, region: str) -> None:
+        """Add a country with the given name to this flight netowrk.
+
+        The new country is not connected by a flight to any other countries.
+        """
+        self.countries[name] = Country(name, {}, region)
+        # TODO: Define a function to find the WHO region the given country is in
+
+    def add_flight(self, country1: str, country2: str) -> None:
+        """Add a flight between the two countries with the given names in this flight network.
+
+        Preconditions:
+            - country1 != country2
+        """
+        if country1 not in self.countries:
+            self.add_country(country1, region1)
+        if country2 not in self.countries:
+            self.add_country(country2, region2)
+
+        f1 = self.countries[country1]
+        f2 = self.countries[country2]
+
+        f1.neighbours[country2] = f2
+        f2.neighbours[country1] = f1
+
+    def connected(self, country1: str, country2: str) -> bool:
+        """Return whether country1 and country2 are countries connected """
+        if country1 in self.countries and country2 in self.countries:
+            c1 = self.countries[country1]
+            return c1.check_connected(country2, set())
+        else:
+            return False
