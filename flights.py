be40ce147ed1734dd11be5ac9dd0d707374208ed
alexnguyen02 <minhtrang.nguyen@mail.utoronto.ca>
2023-03-31 15:59:53 -0400

    again

--- flights.py
+++ flights.py
@@ -32,7 +32,7 @@ class _Country:
         """Initialize this country with the given name, region, and neighbours."""
         self.name = name
         self.neighbours = {}
-        self.safety_index = compute_stats.compute_safety_index(name)
+        self.danger_index = compute_stats.compute_safety_index(name)
         self.add_region()
 
     def add_region(self) -> None:
