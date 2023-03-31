a6f0dd3b9e57229038d0d6949e765265d3a5cc07
duahuss <109004925+duahuss@users.noreply.github.com>
2023-03-31 15:58:45 -0400

    a

--- generate_graph.py
+++ generate_graph.py
@@ -7,9 +7,9 @@ def generate_flight_network(file: str) -> f.Flights():
     flight = f.Flights()
     with open(file) as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
-
         for row in reader:
             source = row[0]
             dest = row[1]
+            if source or dest not in
             flight.add_flight(source, dest)
     return flight
