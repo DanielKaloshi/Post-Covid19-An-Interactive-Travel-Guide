5af8a525d46fb96239a942e94679f93b072876ec
duahuss <109004925+duahuss@users.noreply.github.com>
2023-04-01 18:25:05 -0400

    h

--- generate_graph.py
+++ generate_graph.py
@@ -1,17 +1,19 @@
 """"""
 import csv
 import flights as f
+import filter_file
 
 def generate_flight_network(file: str) -> f.Flights():
     """ """
     flight = f.Flights()
-    #lst_of_countries = filter_file.country_list_UN()
+    lst_of_countries = filter_file.country_list_UN()
     with open(file) as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         for row in reader:
             source = row[0]
             dest = row[1]
-
-
-            flight.add_flight(source, dest)
+            if source or dest not in filter_file:
+                pass
+            else:
+                flight.add_flight(source, dest)
     return flight
