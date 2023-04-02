c999d6556266a3605837e71a2fc44db4386b33d0
alexnguyen02 <minhtrang.nguyen@mail.utoronto.ca>
2023-04-02 19:26:35 -0400

    again

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ def runner() -> None:
     """ Runner to run file user_interactions.py"""
     with open("user_interactions.py") as f:
         exec(f.read())
+
+
+if __name__ == '__main__':
+    user_in
