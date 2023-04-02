091a20dda0c0dcecf0ebc9078b03dd3e4724790b
duahuss <109004925+duahuss@users.noreply.github.com>
2023-04-02 17:46:17 -0400

    a

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 """ A runner file to output our Interactive User Module"""
-import user_interactions
+
+
 def runner() -> None:
-    """ """
+    """ Runner to run file user_interactions.py"""
+    with open("user_interactions.py") as f:
+        exec(f.read())
