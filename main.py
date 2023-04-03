57e53a62c64b613deae1a0b5b92d4650bc82eb32
duahuss <109004925+duahuss@users.noreply.github.com>
2023-04-02 21:42:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """ A runner file to output our Interactive User Module"""
-
-
-def runner() -> None:
-    """ Runner to run file user_interactions.py"""
-    with open("user_interactions.py") as f:
-        exec(f.read())
+# import execfile
+#
+# #!/usr/bin/python
+# import user_interactions
+# # def runner() -> None:
+# #     """ Runner to run file user_interactions.py"""
+# #     with open("user_interactions.py") as f:
+# #         exec(f.read())
+# execfile('user_interactions')
+import os
+os.system("python user_interactions.py")
