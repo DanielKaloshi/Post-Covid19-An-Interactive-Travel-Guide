57b7074ab4661f594d873dde19630730cf5000c7
duahuss <109004925+duahuss@users.noreply.github.com>
2023-04-02 22:59:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-
-""" A runner file to output our Interactive User Module"""
-# import execfile
-#
-# #!/usr/bin/python
-# import user_interactions
-# # def runner() -> None:
-# #     """ Runner to run file user_interactions.py"""
-# #     with open("user_interactions.py") as f:
-# #         exec(f.read())
-# execfile('user_interactions')
+""" A runner file to run user_interaction.py to output our Interactive User Module.
+ This file was not able to call user_interation in a single runner function since user_interatcion uses the
+ library tkinter which layers each function """
 
 # INSTRUCTIONS FOR TA
 
 # -------TO RUN FILE-------
-# 1. PLEASE GO INTO USER_INTERACTIONS.PY RUN AND RUN IN CONSOLE.
+# 1. PLEASE GO INTO USER_INTERACTIONS.PY RUN AND RUN IN CONSOLE. IT SHOULD AUTOMATICALLY OUTPUT THE INTERACTIVE MODULE
 # 2. ENTER NAME, ORIGIN COUNTRY AND DESTINATION COUNTRY (VISIT LIMITATIONS)
 # 3. TO VIEW MAP, PRESS LINK ABOVE
 
